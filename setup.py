291a88a8168a750fe722a7e93570cfe975b6ec62
a1403951401 <1403951401@qq.com>
2019-10-22 14:32:00 +0800

    更新pip源为阿里云

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name="PyIeJs",
-    version="0.0.2",
+    version="0.1.0",
     author="Martin",
     author_email="1403951401@qq.com",
     description="Interop.SHDocVw",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitee.com/Martin-word/PyIeJs",
+    url="https://code.aliyun.com/a1403951401/PyIeJs",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
